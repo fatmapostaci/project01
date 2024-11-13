aaaaf42a7f98380f1d325270ad695bf46bc83693
Ondia <fatmapostaci@gmail.com>
2024-11-13 17:31:42 +0300

    Mentoring Git editlendi_17.31

---
+++ src/GitClass01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GitClass01 {
+    public static void main(String[] args) {
+        Scanner scanf=new Scanner(System.in);
+        int sayi=scanf.nextInt();
+        System.out.println(sayi+"deneme git class");
+    }
+}
---
+++ src/gitPackage/GitClass02_UnderPackage.java
@@ -0,0 +1,8 @@
+package gitPackage;
+
+public class GitClass02_UnderPackage {
+    public static void main(String[] args) {
+        System.out.println("Git Class under the gitPackage");
+
+    }
+}
